5809d0c5a33a5706c810411b741d8edb9eb8cb59
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-11-19 09:00:53 -0700

    Added book labels

--- src/hipster/view/HipsterPanel.java
+++ src/hipster/view/HipsterPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import hipster.controller.HipsterController;
+import hipster.model.Book;
 
 
 public class HipsterPanel extends JPanel
@@ -14,6 +15,12 @@ public class HipsterPanel extends JPanel
 	private HipsterController baseController;
 	private SpringLayout baseLayout;
 	private JComboBox<String> phraseComboBox;
+	private JLabel bookPageCountLabel;
+	private JLabel bookAuthorLabel;
+	private JLabel bookSubjectLabel;
+	private JLabel bookTitleLabel;
+	private JLabel bookPriceLabel;
+	private JButton changeBookButton;
 	private int maxClicks;
 	private int startClick;
 	
@@ -21,7 +28,15 @@ public class HipsterPanel extends JPanel
 	{
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
+		bookPageCountLabel = new JLabel("The page count");
+		bookAuthorLabel = new JLabel("The author");
+		bookPriceLabel = new JLabel("The price");
+		bookSubjectLabel = new JLabel("The subject");
+		bookTitleLabel = new JLabel("The title");
+		changeBookButton = new JButton("Change books");
 		phraseComboBox = new JComboBox<String>();
+		maxClicks = baseController.getFirstHipster().getHipsterBooks().length;
+		startClick = 0;
 	
 
 		setupComboBox();
@@ -45,6 +60,12 @@ public class HipsterPanel extends JPanel
 		this.setLayout(baseLayout);
 		this.setBackground(Color.YELLOW);
 		this.add(phraseComboBox);
+		this.add(bookAuthorLabel);
+		this.add(bookPageCountLabel);
+		this.add(bookTitleLabel);
+		this.add(bookPriceLabel);
+		this.add(bookSubjectLabel);
+		this.add(changeBookButton);
 
 	}
 	
@@ -52,6 +73,18 @@ public class HipsterPanel extends JPanel
 	{
 		baseLayout.putConstraint(SpringLayout.NORTH, phraseComboBox, 24, SpringLayout.NORTH, this);
 		baseLayout.putConstraint(SpringLayout.WEST, phraseComboBox, 30, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, bookAuthorLabel, -168, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, bookPageCountLabel, 0, SpringLayout.EAST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPriceLabel, 6, SpringLayout.SOUTH, bookPageCountLabel);
+		baseLayout.putConstraint(SpringLayout.EAST, bookPriceLabel, 0, SpringLayout.EAST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookPageCountLabel, 6, SpringLayout.SOUTH, bookSubjectLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookSubjectLabel, 6, SpringLayout.SOUTH, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.EAST, bookSubjectLabel, 0, SpringLayout.EAST, bookAuthorLabel);
+		baseLayout.putConstraint(SpringLayout.SOUTH, bookTitleLabel, -180, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, bookTitleLabel, -168, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, bookAuthorLabel, 6, SpringLayout.SOUTH, bookTitleLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, changeBookButton, 6, SpringLayout.SOUTH, bookPriceLabel);
+		baseLayout.putConstraint(SpringLayout.EAST, changeBookButton, 0, SpringLayout.EAST, bookAuthorLabel);
 	}
 	
 	private void setupListeners()
@@ -68,6 +101,36 @@ public class HipsterPanel extends JPanel
 				baseController.getBaseFrame().setTitle(updatedTitle);
 				
 			}
+		});
+		
+	
+		
+		changeBookButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				Book [] tempBooks = baseController.getFirstHipster().getHipsterBooks();
+				if(startClick < maxClicks)
+				{
+					bookSubjectLabel.setText("Book subject:  " + tempBooks[startClick].getSubject());
+					bookAuthorLabel.setText("Book author:  " + tempBooks[startClick].getAuthor());
+					bookTitleLabel.setText("Book title:  " + tempBooks[startClick].getTitle());
+					bookPageCountLabel.setText("Book pages:  " + tempBooks[startClick].getPageCount());
+					bookPriceLabel.setText("Book price:  $" + tempBooks[startClick].getPrice());
+					startClick++;
+				}
+				else
+				{
+					startClick = 0;
+					bookSubjectLabel.setText("Book subject:  ");
+					bookAuthorLabel.setText("Book author:  ");
+					bookTitleLabel.setText("Book title:  ");
+					bookPageCountLabel.setText("Book pages:  ");
+					bookPriceLabel.setText("Book price:  $");
+								
+				}
+			}
+			
 		});
 		
 	}
